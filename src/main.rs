3b465fcea89d739c5f00fd73f6782d7ff56bdb92
aiden_mac <amm926616@gmail.com>
2025-06-02 14:39:08 +0630

    updated modified script

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,21 @@ use clap::Parser;
 use futures::StreamExt;
 use librespot_core::authentication::Credentials;
 use librespot_core::SessionConfig;
-use librespot_core::config::DeviceType;
-use librespot_discovery::Discovery;
+use librespot_discovery::{DeviceType, Discovery};
 use sha1::{Digest, Sha1};
 use serde_json;
 use std::fs::File;
 use std::io::Write;
 use std::process::exit;
-use std::str::FromStr;
 use log::warn;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "Speaker")]
+    #[arg(short, long, default_value = "Generate Credentials")]
     name: String,
     #[arg(short, long, default_value = "credentials.json")]
     path: String,
-    #[arg(short, long, default_value = "speaker")]
-    class: String,
 }
 
 pub fn save_credentials_and_exit(location: &str, cred: &Credentials) {
@@ -44,21 +40,14 @@ async fn main() {
     let name = args.name;
     let credentials_location = args.path;
     let device_id = hex::encode(Sha1::digest(name.clone().as_bytes()));
-    let device_type = match DeviceType::from_str(&args.class) {
-        Ok(device_type) => device_type,
-        Err(_) => {
-            eprintln!("Invalid device type: {}", args.class);
-            exit(1);
-        }
-    };
 
     let mut server = Discovery::builder(device_id.clone(), SessionConfig::default().client_id)
-        .name(name.clone())
-        .device_type(device_type)
+        .name(name.clone())  // Custom device name from args
+        .device_type(DeviceType::Computer)  // Closest to a web player
         .launch()
         .unwrap();
 
-    println!("Open Spotify and select output device: {}", name);
+    println!("Open Spotify and select the output device: Generate Credentials");
 
     while let Some(credentials) = server.next().await {
         save_credentials_and_exit(&credentials_location, &credentials);
